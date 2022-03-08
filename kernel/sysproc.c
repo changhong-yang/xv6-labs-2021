bd7e4bf25f0aab9161f908979371990ed8e712ab
yangchanghong <yangchanghong@protonmail.com>
2022-03-08 18:53:42 -0500

    pass all tests. will work on optional challenges some times later.

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -11,10 +11,10 @@ uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +33,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,11 +44,11 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
-  
+
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -59,13 +59,14 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -75,12 +76,84 @@ sys_sleep(void)
   return 0;
 }
 
-
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
+int sys_pgaccess(void)
 {
+  printf("start pgaccess!\n");
   // lab pgtbl: your code here.
+
+  // Get the parameters
+  uint64 va;
+  if (argaddr(0, &va) < 0)
+    return -1;
+  int num_page;
+  if (argint(1, &num_page) < 0)
+    return -1;
+  uint64 return_address;
+  if (argaddr(2, &return_address) < 0)
+    return -1;
+
+  // buffer stores temporay access bits
+  int num_byte;
+  if (num_page % 8 != 0)
+  {
+    num_byte = num_page / 8 + 1;
+  }
+  else
+  {
+    num_byte = num_page / 8;
+  }
+  char buf[num_byte];
+  for (int i = 0; i < num_byte; i++)
+  {
+    buf[i] = '\0';
+  }
+
+  for (int i = 0; i < num_page; i++)
+  {
+    // printf("calcuate addr\n");
+    uint64 addr = va + i * PGSIZE;
+    pagetable_t page = myproc()->pagetable;
+    // modified from walk()
+    // printf("start walking\n");
+    for (int level = 2; level > 0; level--)
+    {
+      // printf("walk at level %d\n", level);
+      pte_t *pte = &page[PX(level, addr)];
+      if (*pte & PTE_V)
+      {
+        page = (pagetable_t)PTE2PA(*pte);
+      }
+      else
+      {
+        continue;
+      }
+    }
+
+    // check access bit
+    if ((page[PX(0, addr)] & PTE_V) && (page[PX(0, addr)] & PTE_A))
+    {
+      printf("access bit %d\n", i);
+      int d, r = i % 8;
+      if (r == 0)
+      {
+        d = i / 8 + 1;
+      }
+      else
+      {
+        d = i / 8;
+      }
+      buf[d] = buf[d] | (1 << r);
+      page[PX(0, addr)] = page[PX(0, addr)] & (~PTE_A); // turn off access bit
+    }
+  }
+
+  uint32 tem = (int)*buf;
+  printf("%x \n", tem);
+
+  if (copyout(myproc()->pagetable, return_address, buf, num_byte) < 0)
+    return -1;
+  printf("done\n");
   return 0;
 }
 #endif
@@ -90,7 +163,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
